4acd0f9bd4abe48c8c2f04518903bf1d28fd9883
Daniel Beecham <daniel@lunix.se>
2021-12-06 09:44:06 +0100

    gotten tired of these debug logs

--- st.c
+++ st.c
@@ -1684,8 +1684,8 @@ csihandle(void)
 	switch (csiescseq.mode[0]) {
 	default:
 	unknown:
-		fprintf(stderr, "erresc: unknown csi ");
-		csidump();
+//		fprintf(stderr, "erresc: unknown csi ");
+//		csidump();
 		/* die(""); */
 		break;
 	case '@': /* ICH -- Insert <n> blank char */
@@ -1986,8 +1986,8 @@ strhandle(void)
 		return;
 	}
 
-	fprintf(stderr, "erresc: unknown str ");
-	strdump();
+//	fprintf(stderr, "erresc: unknown str ");
+//	strdump();
 }
 
 void
